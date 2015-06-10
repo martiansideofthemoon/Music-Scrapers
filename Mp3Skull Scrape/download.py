6a36b042dc5a4a81b9b261139c447f2b341282c0
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-06-11 03:49:08 +0530

    basic song details scraping

--- Mp3Skull Scrape/download.py
+++ Mp3Skull Scrape/download.py
@@ -9,8 +9,9 @@ def checkInvalid(songName=None,checkCover=True,checkRemix=True):
 	if not remixMatch.search(songName)==None and checkRemix==True:
 		isValid=False
 	return isValid
-
-
+def getSongDetails(query=""):
+	songDetails = {'search_query':query}
+	return songDetail
 query=""
 i=0
 for part in sys.argv[1:]:
@@ -19,30 +20,32 @@ query=query[:len(query)-1]
 br = mechanize.Browser()
 br.set_handle_robots( False )
 br.addheaders = [('User-agent', 'Firefox')]
-br.open('http://mp3skull.cr')
-for form in br.forms():
-	if form.attrs['id']=='f1':
-		br.form = form
-		break
-br['q'] = query
-response = br.submit()
-finalpage = bs4.BeautifulSoup(response.read())
-maindivs=[]
-tryNumber=1
-for div in finalpage.select('div'):
-	desiredExpression = re.compile(r'show\d+')
-	if 'class' in div.attrs:
-		isFound = desiredExpression.search(div.attrs['class'][0])
-		if not isFound == None:
-			maindivs.append(div)
-for maindiv in maindivs:
-	songTitle = maindiv.find('b').getText()
-	isValid = checkInvalid(songTitle,True,True)
-	if isValid==True:
-		links=maindiv.find_all('a')
-		wget = os.system("wget \""+links[0].get('href')+"\" -P ~/Music/")
-		if wget==0:
+try:
+	br.open('http://mp3skull.cr')
+	for form in br.forms():
+		if form.attrs['id']=='f1':
+			br.form = form
 			break
+	br['q'] = query
+	response = br.submit()
+	finalpage = bs4.BeautifulSoup(response.read())
+	maindivs=[]
+	for div in finalpage.select('div'):
+		desiredExpression = re.compile(r'show\d+')
+		if 'class' in div.attrs:
+			isFound = desiredExpression.search(div.attrs['class'][0])
+			if not isFound == None:
+				maindivs.append(div)
+	for maindiv in maindivs:
+		songTitle = maindiv.find('b').getText()
+		isValid = checkInvalid(songTitle,True,True)
+		if isValid==True:
+			links=maindiv.find_all('a')
+			wget = os.system("wget \""+links[0].get('href')+"\" -P ~/Music/")
+			if wget==0:
+				break
+except:
+	print "Please check your internet connection. Or else, try again later."
 
 
 
