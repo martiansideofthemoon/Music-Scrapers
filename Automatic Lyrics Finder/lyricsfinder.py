dbf26bae5e7670c0f923f0b6673f380632bfc24d
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-06-12 22:00:13 +0530

    Error handling updated

--- Automatic Lyrics Finder/lyricsfinder.py
+++ Automatic Lyrics Finder/lyricsfinder.py
@@ -4,6 +4,9 @@ i=0
 for part in sys.argv[1:]:
 	query=query+part+'+'
 query=query[:len(query)-1]
+if query.strip()=="":
+    print "Please write a query."
+    exit()
 br = mechanize.Browser()
 br.set_handle_robots( False )
 br.addheaders = [('User-agent', 'Firefox')]
